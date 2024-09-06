1c3563c31ff3e79bd254cd2cd30109a0f286ab8b
leebs0521 <leebs0521@gmail.com>
2024-09-06 15:19:16 +0900

    feat: initial commit

---
+++ src/main/java/org/prgrms/coffee_order_be/CoffeeOrderBeApplication.java
@@ -0,0 +1,13 @@
+package org.prgrms.coffee_order_be;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeOrderBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoffeeOrderBeApplication.class, args);
+	}
+
+}
